fc905cd3df89dd5b78c275e7568b88719dd74244
danielecostabnu-dev <danielecosta.bnu@gmail.com>
2026-03-14 23:35:11 -0300

    melhorando análise de dados

--- analise_dados.py
+++ analise_dados.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
 # carregar os dados
-dados = pd.read_csv("../dados/empreendedorismo_sc.csv")
+dados = pd.read_csv("dados/empreendedorismo_sc.csv")
 
 print("Primeiras linhas do dataset:")
 print(dados.head())
+
+print("\nInformações do dataset:")
+print(dados.info())
+
+print("\nResumo estatístico:")
+print(dados.describe())
 
 # análise por cidade
 empresas_cidade = dados.groupby("cidade")["empresas"].sum()
@@ -17,3 +23,9 @@ empresas_setor = dados.groupby("setor")["empresas"].sum()
 
 print("\nEmpresas por setor:")
 print(empresas_setor)
+
+# cidade com mais empresas
+cidade_top = dados.loc[dados["empresas"].idxmax()]
+
+print("\nCidade com mais empresas:")
+print(cidade_top)
